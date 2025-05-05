9e4c4c632ac5d6eb46db2a4483cc6089edfc6ea9
TrainingDataSet <161457914+TrainingDataSet@users.noreply.github.com>
2025-05-05 16:02:08 +0900

    test: 부동산 데이터 수집 테스트
    주택매매가격지수, 주택전세가격지수, 주택월세통합가격지수

--- modules/collect_economic_data.py
+++ modules/collect_economic_data.py
@@ -2,9 +2,9 @@ import os
 import json
 import requests
 import pandas as pd
-import streamlit as st
 from dotenv import load_dotenv
 from fredapi import Fred
+from itertools import product
 
 # .env 파일의 환경 변수 불러오기
 load_dotenv()
@@ -99,4 +99,59 @@ class collectEconomicData:
             dataset = dataset.join(df, how="left")
 
         dataset = dataset.reset_index()
+        return dataset
+
+    def real_estate(self, start, end, code_lst, freq):
+
+        dataset = pd.DataFrame(index=pd.date_range(start + "01", end + "01", freq="MS").strftime("%Y%m"))
+        dataset.index.name = "time"
+        dataset.index = pd.to_datetime(dataset.index, format='%Y%m').to_period('M')
+
+        for code in code_lst:
+            url = f"http://ecos.bok.or.kr/api/StatisticSearch/{self.ecos_api_key}/json/kr/1/10000/{code[0]}/{freq}/{start}/{end}/{code[1]}/{code[2]}"
+            r = requests.get(url)
+            jo = json.loads(r.text)
+            result = pd.json_normalize(jo['StatisticSearch']['row'])
+            if code[0] in ("901Y009", "404Y014"):
+                data_name = pd.unique(result["STAT_NAME"])[0]
+            else:
+                data_name = pd.unique(result["ITEM_NAME1"])[0]
+
+            df = result[["TIME", "DATA_VALUE"]]
+            df = df.rename(columns={"TIME": "time", "DATA_VALUE": data_name})
+            df[data_name] = pd.to_numeric(df[data_name])
+            df['time'] = pd.to_datetime(df['time'], format='%Y%m').dt.to_period('M')
+            df = df.set_index("time")
+            dataset = dataset.join(df, how="left", rsuffix=".x")
+
+        dataset = dataset.reset_index()
+
+        list1 = ["sale", "jeon", "month"]  # 매매, 전세, 월세
+        list2 = ["apt", "row", "det"]  # 아파트, 연립다세대, 단독주택
+        list3 = [
+            "S",  # 서울
+            "GG",  # 경기
+            "IC",  # 인천
+            "BS",  # 부산
+            "DG",  # 대구
+            "GJ",  # 광주
+            "DJ",  # 대전
+            "US",  # 울산
+            "SJ",  # 세종
+            "GW",  # 강원
+            "CB",  # 충북
+            "CN",  # 충남
+            "JB",  # 전북
+            "JN",  # 전남
+            "GB",  # 경북
+            "GN",  # 경남
+            "JJ"  # 제주
+        ]
+
+        # 새로운 컬럼명
+        new_col = [f"{a}_{b}_{c}" for a, b, c in product(list1, list2, list3)]
+        new_col.insert(0, "time")
+
+        dataset.columns = new_col
+
         return dataset
--- run_collector.py
+++ run_collector.py
@@ -3,6 +3,7 @@ from modules.DB import SupabaseDB
 from datetime import datetime
 import pytz
 from dateutil.relativedelta import relativedelta
+from itertools import product
 
 # 클래스 초기화
 db = SupabaseDB()
@@ -11,65 +12,81 @@ collect_economic_data = collectEconomicData()
 # 한국 시간 설정
 kst = pytz.timezone("Asia/Seoul")
 
-#------------------ 최근 10년간 일별 국내 데이터 ------------------#
+# #------------------ 최근 10년간 일별 국내 데이터 ------------------#
+# # 수집 날짜
+# start = (datetime.now(kst) - relativedelta(years=10)).strftime("%Y%m%d")
+# end = datetime.now(kst).strftime("%Y%m%d")
+#
+# # 국고채(3년), 국고채(10년), 기준금리, KOSPI, KOSDAQ, 원/달러 환율
+# daily_domestic_code_lst = [
+#     ("817Y002", "010200000"), ("817Y002", "010210000"), ("722Y001", "0101000"), ("802Y001", "0001000"),
+#     ("802Y001", "0089000"), ("731Y001", "0000001")
+# ]
+# freq = "D"  # 일별
+# code_dict = {"국고채(3년)": "kr_bond_3y", "국고채(10년)": "kr_bond_10y",
+#              "한국은행 기준금리": "kr_base_rate", "KOSPI지수": "kospi",
+#              "KOSDAQ지수": "kosdaq", "원/미국달러(매매기준율)": "usd_krw"}
+#
+# dataset_daily = collect_economic_data.daily_domestic(start, end, daily_domestic_code_lst, freq, code_dict)
+# db.insert_economic(dataset_daily, "domestic_daily_economic")
+#
+#
+# #------------------ 최근 10년간 월별 국내 데이터 ------------------#
+# # 수집 날짜
+# start = (datetime.now(kst) - relativedelta(years=10)).strftime("%Y%m")
+# end = datetime.now(kst).strftime("%Y%m")
+#
+# # 실업률, 고용률, 소비자물가지수, 생산자물가지수, 경상수지
+# monthly_domestic_code_lst = [
+#     ("901Y027", "I61BC/I28B"), ("901Y027", "I61E/I28B"), ("901Y009", "0"), ("404Y014", "*AA"), ("301Y017", "SA000")
+# ]
+# freq = "M"  # 월별
+# code_dict = {"실업률": "unemp_rate", "고용률": "emp_rate", "4.2.1. 소비자물가지수": "cpi",
+#              "4.1.1.1. 생산자물가지수(기본분류)": "ppi", "경상수지": "curr_account"}
+#
+# dataset_monthly = collect_economic_data.monthly_domestic(start, end, monthly_domestic_code_lst, freq, code_dict)
+# db.insert_economic(dataset_monthly, "domestic_monthly_economic")
+#
+#
+# #------------------ 최근 10년간 일별 미국 데이터 ------------------#
+# # 수집 날짜
+# start = (datetime.today() - relativedelta(years=10)).strftime("%Y%m%d")
+# end = datetime.today().strftime("%Y%m%d")
+#
+# # 2년물 미국채 금리, 10년물 미국채 금리, 미국 기준금리(FFR), S&P500, 나스닥, 다우존스
+# code_lst = ["DGS2", "DGS10", "DFEDTARU", "SP500", "NASDAQCOM", "DJIA"]
+# code_dict = {"DGS2": "us_bond_2y", "DGS10": "us_bond_10y", "DFEDTARU": "ffr",
+#              "SP500": "sp500", "NASDAQCOM": "nasdaq", "DJIA": "dji"}
+#
+# us_daily = collect_economic_data.daily_us(start, end, code_lst, code_dict)
+# db.insert_economic(us_daily, "us_daily_economic")
+#
+#
+# #------------------ 최근 10년간 월별 미국 데이터 ------------------#
+# # 수집 날짜
+# start = (datetime.today() - relativedelta(years=10)).strftime("%Y%m%d")
+# end = datetime.today().strftime("%Y%m%d")
+#
+# # 미국 실업률, 미국 비농업고용자수, 미국 Core PCE, 미국 PCE, 미국 CPI, 미국 PPI
+# code_lst = ["UNRATE", "PAYEMS", "PCEPILFE", "PCE", "CPIAUCSL", "PPIACO"]
+# code_dict = {"UNRATE": "us_unemp_rate", "PAYEMS": "us_nfp", "PCEPILFE": "us_core_pce",
+#              "PCE": "us_pce", "CPIAUCSL": "us_cpi", "PPIACO": "us_ppi"}
+#
+# us_monthly = collect_economic_data.monthly_us(start, end, code_lst, code_dict)
+# db.insert_economic(us_monthly, "us_monthly_economic")
+
+
+#------------------ 최근 2년간 부동산 데이터 ------------------#
 # 수집 날짜
-start = (datetime.now(kst) - relativedelta(years=10)).strftime("%Y%m%d")
-end = datetime.now(kst).strftime("%Y%m%d")
+start = (datetime.today() - relativedelta(years=2)).strftime("%Y%m")
+end = datetime.today().strftime("%Y%m")
+freq = "M" # 월별
 
-# 국고채(3년), 국고채(10년), 기준금리, KOSPI, KOSDAQ, 원/달러 환율
-daily_domestic_code_lst = [
-    ("817Y002", "010200000"), ("817Y002", "010210000"), ("722Y001", "0101000"), ("802Y001", "0001000"),
-    ("802Y001", "0089000"), ("731Y001", "0000001")
-]
-freq = "D"  # 일별
-code_dict = {"국고채(3년)": "kr_bond_3y", "국고채(10년)": "kr_bond_10y",
-             "한국은행 기준금리": "kr_base_rate", "KOSPI지수": "kospi",
-             "KOSDAQ지수": "kosdaq", "원/미국달러(매매기준율)": "usd_krw"}
+# 주택매매가격지수, 주택전세가격지수, 주택월세통합가격지수
+list1 = ["901Y093", "901Y094", "901Y095"]
+list2 = ["H69B", "H69C", "H69D"]
+list3 = ["R70F", "R70G", "R70H", "R70I", "R70J", "R70K", "R70L", "R70M", "R70N", "R70O", "R70P", "R70Q", "R70R", "R70S", "R70T", "R70U", "R70V"]
+code_lst = list(product(list1, list2, list3))
 
-dataset_daily = collect_economic_data.daily_domestic(start, end, daily_domestic_code_lst, freq, code_dict)
-db.insert_economic(dataset_daily, "domestic_daily_economic")
-
-
-#------------------ 최근 10년간 월별 국내 데이터 ------------------#
-# 수집 날짜
-start = (datetime.now(kst) - relativedelta(years=10)).strftime("%Y%m")
-end = datetime.now(kst).strftime("%Y%m")
-
-# 실업률, 고용률, 소비자물가지수, 생산자물가지수, 경상수지
-monthly_domestic_code_lst = [
-    ("901Y027", "I61BC/I28B"), ("901Y027", "I61E/I28B"), ("901Y009", "0"), ("404Y014", "*AA"), ("301Y017", "SA000")
-]
-freq = "M"  # 월별
-code_dict = {"실업률": "unemp_rate", "고용률": "emp_rate", "4.2.1. 소비자물가지수": "cpi",
-             "4.1.1.1. 생산자물가지수(기본분류)": "ppi", "경상수지": "curr_account"}
-
-dataset_monthly = collect_economic_data.monthly_domestic(start, end, monthly_domestic_code_lst, freq, code_dict)
-db.insert_economic(dataset_monthly, "domestic_monthly_economic")
-
-
-#------------------ 최근 10년간 일별 미국 데이터 ------------------#
-# 수집 날짜
-start = (datetime.today() - relativedelta(years=10)).strftime("%Y%m%d")
-end = datetime.today().strftime("%Y%m%d")
-
-# 2년물 미국채 금리, 10년물 미국채 금리, 미국 기준금리(FFR), S&P500, 나스닥, 다우존스
-code_lst = ["DGS2", "DGS10", "DFEDTARU", "SP500", "NASDAQCOM", "DJIA"]
-code_dict = {"DGS2": "us_bond_2y", "DGS10": "us_bond_10y", "DFEDTARU": "ffr",
-             "SP500": "sp500", "NASDAQCOM": "nasdaq", "DJIA": "dji"}
-
-us_daily = collect_economic_data.daily_us(start, end, code_lst, code_dict)
-db.insert_economic(us_daily, "us_daily_economic")
-
-
-#------------------ 최근 10년간 월별 미국 데이터 ------------------#
-# 수집 날짜
-start = (datetime.today() - relativedelta(years=10)).strftime("%Y%m%d")
-end = datetime.today().strftime("%Y%m%d")
-
-# 미국 실업률, 미국 비농업고용자수, 미국 Core PCE, 미국 PCE, 미국 CPI, 미국 PPI
-code_lst = ["UNRATE", "PAYEMS", "PCEPILFE", "PCE", "CPIAUCSL", "PPIACO"]
-code_dict = {"UNRATE": "us_unemp_rate", "PAYEMS": "us_nfp", "PCEPILFE": "us_core_pce",
-             "PCE": "us_pce", "CPIAUCSL": "us_cpi", "PPIACO": "us_ppi"}
-
-us_monthly = collect_economic_data.monthly_us(start, end, code_lst, code_dict)
-db.insert_economic(us_monthly, "us_monthly_economic")
+real_estate = collect_economic_data.real_estate(start, end, code_lst, freq)
+db.insert_economic(real_estate, "real_estate")
