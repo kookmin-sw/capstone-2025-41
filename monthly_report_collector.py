c37e61c9a629c7b44e142363ba5c4197fd3dbadd
TrainingDataSet <161457914+TrainingDataSet@users.noreply.github.com>
2025-05-09 19:07:34 +0900

    test: 부동산 동향 리포트 제외

--- monthly_report_collector.py
+++ monthly_report_collector.py
@@ -1,5 +1,5 @@
 from modules.AI_report import create_real_estate_report, create_macro_report
 
 #------------------ AI 리포트 생성 ------------------#
-create_real_estate_report()  # 부동산 동향 리포트
+# create_real_estate_report()  # 부동산 동향 리포트
 create_macro_report()  # 거시경제 동향 리포트
